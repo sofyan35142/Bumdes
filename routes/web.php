0cba26bcba037ece478fa8c9f33504fd07723043
Jukik45 <unluading123@gmail.com>
2025-08-02 12:36:13 +0700

    rapkan route

--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,73 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Beranda;
-use App\Http\Controllers\Admin\visiMisi;
+use App\Http\Controllers\Admin\VisiMisi;
+use App\Http\Controllers\Landingpage\Home;
 use App\Http\Controllers\Landingpage\Blog;
-use App\Http\Controllers\Landingpage\home;
 use App\Http\Controllers\Landingpage\Profile;
 use App\Http\Controllers\Landingpage\Contact;
-use App\Http\Controllers\Landingpage\pages;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Landingpage\Pages;
 
-// Route::get('/', function () {
-//     return view('Landingpage.index');
-// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Terbagi menjadi dua menu utama:
+| 1. Landing Page (Publik)
+| 2. Admin (Panel Backend)
+|
+*/
 
-Route::get('/', [home::class, 'home'])->name('home');
 
+// ==========================================================
+// =============== 1. LANDING PAGE / PUBLIK =================
+// ==========================================================
+
+// --- Beranda
+Route::get('/', [Home::class, 'home'])->name('home');
+
+// --- Blog
 Route::get('/blog', [Blog::class, 'blog'])->name('blog');
 Route::get('/blogdetail', [Blog::class, 'blogdetail'])->name('blogdetail');
 
+// --- Profil BUMDes
 Route::get('/profile/tentangBumdes', [Profile::class, 'tentangBumdes'])->name('profile.tentangBumdes');
 Route::get('/profile/visimisi', [Profile::class, 'visiMisi'])->name('profile.visiMisi');
 Route::get('/profile/strukturorganisasi', [Profile::class, 'strukturorganisasi'])->name('profile.strukturorganisasi');
 Route::get('/profile/dasarhukum', [Profile::class, 'dasarhukum'])->name('profile.dasarhukum');
 
+// --- Halaman Tambahan
+Route::get('/galeri', [Pages::class, 'galeri'])->name('galeri');
+Route::get('/lowongan', [Pages::class, 'lowongan'])->name('lowongan');
+Route::get('/pages/galeri', [Pages::class, 'galeri'])->name('pages.galeri');
+Route::get('/pages/lowongan', [Pages::class, 'lowongan'])->name('pages.lowongan');
+Route::get('/pages/keuangan', [Pages::class, 'keuangan'])->name('pages.keuangan');
 
+// --- Kontak
 Route::get('/contact', [Contact::class, 'contact'])->name('contact');
 
 
 
-Route::get('/galeri', [pages::class, 'galeri'])->name('galeri');
-Route::get('/lowongan', [pages::class, 'lowongan'])->name('lowongan');
-Route::get('/pages/galeri', [pages::class, 'galeri'])->name('galeri');
-Route::get('/pages/lowongan', [pages::class, 'lowongan'])->name('lowongan');
-Route::get('/pages/keuangan', [pages::class, 'keuangan'])->name('keuangan');
+// ==========================================================
+// ===================== 2. ADMIN PANEL =====================
+// ==========================================================
 
-
+// --- Dashboard Admin
 Route::get('/admin', [Beranda::class, 'beranda'])->name('admin.beranda');
 
-<<<<<<< HEAD
-Route::get('/admin/visi-misi', [visiMisi::class, 'index'])->name('admin.visiMisi');
+// --- Visi & Misi
+Route::get('/admin/visi-misi', [VisiMisi::class, 'index'])->name('admin.visiMisi');
 Route::get('/admin/visi-misi/edit', [VisiMisi::class, 'edit'])->name('admin.visi_misi.edit');
 Route::put('/admin/visi-misi/update', [VisiMisi::class, 'edit'])->name('admin.visi_misi.update');
-=======
-//////////////////////////////SLIDERRRRRRRRRRRRRRR////////////////////////////////////////
+
+// --- Slider
 Route::get('/admin/slider', [Beranda::class, 'slider'])->name('admin.slider');
 Route::get('/admin/editslider/{id}', [Beranda::class, 'editslider']);
 Route::post('/admin/updateslider/{id}', [Beranda::class, 'updateslider'])->name('admin.updateslider');
-//////////////////////////////Sambutan DIrektur/////////////////////////////////////////
+
+// --- Sambutan Direktur
 Route::get('/admin/sambutan', [Beranda::class, 'sambutan'])->name('admin.sambutan');
 Route::get('/admin/editsambutan/{id}', [Beranda::class, 'editsambutan']);
 Route::post('/admin/updatesambutan/{id}', [Beranda::class, 'updatesambutan'])->name('admin.updatesambutan');
-
-
->>>>>>> 09828339ac63708b7be75e014660edb70ca4c781
