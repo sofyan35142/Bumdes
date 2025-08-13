7e8c741d5a4dd3e6b52745d37435e5a6d281750e
sofyan35142 <sofyan35142@gmail.com>
2025-08-13 20:02:29 +0700

    up

--- resources/views/Landingpage/formtestimonial.blade.php
+++ resources/views/Landingpage/formtestimonial.blade.php
@@ -89,15 +89,19 @@
                                     </div>
 
                                     {{-- Nomor Telepon --}}
-                                    <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                                        <input type="tel" name="nomor_telepone" placeholder="Nomor Telepon"
-                                            value="{{ old('nomor_telepone') }}"
-                                            oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0,13)"
-                                            pattern="[0-9]{10,13}" inputmode="numeric" required>
-                                        @error('nomor_telepone')
-                                            <small class="text-danger">{{ $message }}</small>
-                                        @enderror
-                                    </div>
+<div class="col-lg-6 col-md-6 col-sm-12 form-group">
+    <input type="number"
+           name="nomor_telepone"
+           placeholder="Nomor Telepon"
+           value="{{ old('nomor_telepone') }}"
+           oninput="this.value = this.value.replace(/[^0-9]/g,'').slice(0,13)"
+           onblur="if(this.value.length < 10){ alert('Nomor telepon minimal 10 digit'); this.focus(); }"
+           required>
+    @error('nomor_telepone')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
+</div>
+
 
 
                                     {{-- Keterangan --}}
