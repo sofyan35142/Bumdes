8fdb1583e2cecce87d2b6f61de65cfcc74954369
sofyan35142 <sofyan35142@gmail.com>
2025-08-14 13:14:49 +0700

    testimoni

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -19,33 +19,33 @@ class TestimonialController extends Controller
     public function store(Request $request)
     {
         // dd($request->all());
-        $request->validate([
-            'nama' => 'required|string|max:255',
-            'keterangan' => 'required|string|max:255',
-            'deskripsi_testimonial' => 'required|string|max:1000',
-            'nomor_telepone' => 'required|numeric',
-            'foto_testimonial' => 'required|image|mimes:jpeg,png,jpg|max:2048'
-        ], [
-            'nama.required' => 'Nama wajib diisi.',
-            'nama.string' => 'Nama harus berupa teks.',
-            'nama.max' => 'Nama maksimal 255 karakter.',
+        // $request->validate([
+        //     'nama' => 'required|string|max:255',
+        //     'keterangan' => 'required|string|max:255',
+        //     'deskripsi_testimonial' => 'required|string|max:1000',
+        //     'nomor_telepone' => 'required|numeric',
+        //     'foto_testimonial' => 'required|image|mimes:jpeg,png,jpg|max:2048'
+        // ], [
+        //     'nama.required' => 'Nama wajib diisi.',
+        //     'nama.string' => 'Nama harus berupa teks.',
+        //     'nama.max' => 'Nama maksimal 255 karakter.',
 
-            'keterangan.required' => 'Keterangan wajib diisi.',
-            'keterangan.string' => 'Keterangan harus berupa teks.',
-            'keterangan.max' => 'Keterangan maksimal 255 karakter.',
+        //     'keterangan.required' => 'Keterangan wajib diisi.',
+        //     'keterangan.string' => 'Keterangan harus berupa teks.',
+        //     'keterangan.max' => 'Keterangan maksimal 255 karakter.',
 
-            'deskripsi_testimonial.required' => 'Deskripsi testimonial wajib diisi.',
-            'deskripsi_testimonial.string' => 'Deskripsi testimonial harus berupa teks.',
-            'deskripsi_testimonial.max' => 'Deskripsi testimonial maksimal 1000 karakter.',
+        //     'deskripsi_testimonial.required' => 'Deskripsi testimonial wajib diisi.',
+        //     'deskripsi_testimonial.string' => 'Deskripsi testimonial harus berupa teks.',
+        //     'deskripsi_testimonial.max' => 'Deskripsi testimonial maksimal 1000 karakter.',
 
-            'nomor_telepone.required' => 'Nomor telepon wajib diisi.',
-            'nomor_telepone.numeric' => 'Nomor telepon harus berupa angka.',
+        //     'nomor_telepone.required' => 'Nomor telepon wajib diisi.',
+        //     'nomor_telepone.numeric' => 'Nomor telepon harus berupa angka.',
 
-            'foto_testimonial.required' => 'Foto testimonial wajib diunggah.',
-            'foto_testimonial.image' => 'File harus berupa gambar.',
-            'foto_testimonial.mimes' => 'Foto harus berformat jpeg, png, atau jpg.',
-            'foto_testimonial.max' => 'Ukuran foto maksimal 2MB.'
-        ]);
+        //     'foto_testimonial.required' => 'Foto testimonial wajib diunggah.',
+        //     'foto_testimonial.image' => 'File harus berupa gambar.',
+        //     'foto_testimonial.mimes' => 'Foto harus berformat jpeg, png, atau jpg.',
+        //     'foto_testimonial.max' => 'Ukuran foto maksimal 2MB.'
+        // ]);
 
 
         $testimonial = new Testimonial;
